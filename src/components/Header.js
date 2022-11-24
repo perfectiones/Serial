9e83a01cf27e4e5251ec16154f16e5fc8ad691db
dmitriy oginov <dmitrij.oginov@mail.ru>
2022-11-24 12:14:40 +0300

    add adaptive header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,25 +3,38 @@ import '../scss/Header.scss';
 
 function Header() {
     return(
-        /*  Заменить LOGO, image должен быть не вставлен в logo, а быть background-image*/
 
         <header>
-            <div className="container header">
-                <div className="header__logo" id='logo'>  
-                    <label for="logo">serial.com</label>
-                </div>
-                <div className="header__item">
-                    <div className="item__list">
-                        <ul>
-                            <li className='active'>Актеры</li>
-                            <li>Новости</li>
-                            <li id="episode">Серии</li>
-                            <div className='dropdawn' for="episode">
-                                <p>1 Сезон</p>
-                                <p>Случайная серия</p>
-                            </div>
-                        </ul>
+            <div className="container ">
+                <div className='row header'>
+                    <div className='col-lg-5 col-10'>   
+                        <div className="header__logo" id='logo'>  
+                            <label for="logo">serial.com</label>
+                        </div>
                     </div>
+                    
+                    <div className="header__item col-lg-7">
+                            <div className="item__list">
+                                <ul>
+                                    <li className='active'>Актеры</li>
+                                    <li>Новости</li>
+                                    <li id="episode">Серии
+                                        <div className='dropdawn' for="episode">
+                                            <p>1 Сезон</p>
+                                            <p>Случайная серия</p>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                    </div>
+                    <div className='col-2 header__burger'>
+                        <div className='burger__wrapper'>
+                            <span className='line top'></span>
+                            <span className='line middle'></span>
+                            <span className='line bottom'></span>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </header>
